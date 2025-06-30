de6aac9ce311ba785adf2cf407c3fe0e52298da8
Fugguri <fygguri@icloud.com>
2025-06-30 18:09:48 +0300

    update README.MD Add more comments and english language

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="brief_survey",
-    version="0.1.0",
+    version="0.1.3",
     description="Dynamic survey/dialog with aiogram_dialog and Pydantic support",
     author="Fugguri",
     url="https://github.com/Fugguri/brief_survey",
-
     packages=find_packages(),
     install_requires=[
         "aiogram==3.20",
