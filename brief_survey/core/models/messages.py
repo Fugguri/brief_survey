d3104159f5ff8e7a18ebaf2e4b2a84c0d5a119cf
Fugguri <fygguri@icloud.com>
2025-07-03 16:13:48 +0300

    init update

---
+++ brief_survey/core/models/messages.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+
+
+class InfoMessages(BaseModel):
+    invalid_input:str = Field(default="Пожалуйста, введите корректные данные.",)
+    save_success:str = Field(default="Спасибо! Данные успешно сохранены.",)
+    save_fail:str = Field(default="Произошла ошибка при сохранении. Попробуйте позже.",)
+    finish_text:str = Field(default="Данные приняты.",)
+    question_not_found:str = Field(default= "Ошибка: вопрос не найден.")
+
+    class Config:
+        from_attributes=True
