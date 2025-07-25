e641b606bf89456f315e8e19fe4c3fbcce439183
Fugguri <fygguri@icloud.com>
2025-07-25 20:00:51 +0300

    update person phone validator

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="brief_survey",
-    version="0.2.3",
+    version="0.2.2.1",
     description="Dynamic survey/dialog with aiogram_dialog and Pydantic support",
     author="Fugguri",
     url="https://github.com/Fugguri/brief_survey",
     packages=find_packages(),
     install_requires=[
-        "aiogram",
-        "aiogram_dialog==2.3.1",
-        "phonenumbers==9.0.10",
-        "pydantic==2.11.7",
+        "aiogram>=3.20",
+        "aiogram_dialog>=2.3.1",
+        "phonenumbers>=9.0.10",
+        "pydantic>=2.11.7",
     ],
     python_requires='>=3.12',
 
