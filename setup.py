9412bd12e58229cdef2752ee9786d4f38fc2eeaf
Fugguri <fygguri@icloud.com>
2025-08-12 19:50:52 +0300

    fix reqs

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name="brief_survey",
-    version="0.2.4",
+    version="0.2.5",
     description="Dynamic survey/dialog for aiogram3  with aiogram_dialog and Pydantic support",
     author="Fugguri",
     url="https://github.com/Fugguri/brief_survey",
